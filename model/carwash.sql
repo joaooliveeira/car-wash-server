43b0ad3523d674a1717d379ba871dc116eb3b582
João Guilherme <joaog.oliveira96@gmail.com>
2020-09-17 23:17:32 -0300

    last driver done

--- model/carwash.sql
+++ model/carwash.sql
@@ -1,10 +1,12 @@
 SELECT * FROM carwash.client WHERE name = "Joo";
 
-SELECT * FROM carwash.client WHERE id = "ab7d3200-9645-11ea-910e-e32faa846721";
+SELECT * FROM carwash.car WHERE id = "f23d5330-cc1b-11ea-916e-45ff0ba860cc";
 
 select * from carwash.client;
 
 select * from carwash.wash;
+
+select * from carwash.wash WHERE carwash.wash.client_register != "";
 
 select * from carwash.car;
 
